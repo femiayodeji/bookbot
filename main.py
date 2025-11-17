36212af574df23031b57a5bf15bd7d59e711cfee
femiayodeji <femidotayodeji@gmail.com>
2025-11-17 14:12:27 +0100

    Data analysis

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("greetings boots")
+def get_book_text(filepath):
+    with open(filepath) as f:
+        return f.read()
+    
+def count_words(text):
+    words = text.split()
+    return len(words)    
+
+def main():
+    book_text = get_book_text("books/frankenstein.txt")
+    print(f"Found {count_words(book_text)} total words")
+
+main()
